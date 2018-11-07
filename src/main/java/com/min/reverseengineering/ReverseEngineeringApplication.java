0b1f226698097b3140b3b91a5c3196a3a6497a1b
minchenyang <minchenyang>
2018-11-07 20:36:58 +0800

    Initial commit

---
+++ src/main/java/com/min/reverseengineering/ReverseEngineeringApplication.java
@@ -0,0 +1,38 @@
+package com.min.reverseengineering;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseEngineeringApplication {
+
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件
+		String mount = this.getClass().getClassLoader().getResource("generatorConfig.xml").getPath();
+		File configFile = new File(mount);
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	}
+	public static void main(String[] args) throws Exception {
+		try {
+			ReverseEngineeringApplication generatorSqlmap = new ReverseEngineeringApplication();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
